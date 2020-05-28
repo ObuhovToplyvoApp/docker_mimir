35dab5d66f3b2886dadea5b5ae696022f7ebfd1b
Серега Обухов <obuhov.s@toplyvo.app>
2020-05-28 10:46:08 +0300

    Run scripts ver 0.2

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-cp .env.example .env;
+#cp .env.example .env;
 python3 -m pipenv install --python=/usr/bin/python3;
 python3 -m pipenv run inv -f docker_settings.yaml load-in-docker-and-test;
 docker-compose up -d;
